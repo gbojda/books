449ef1b44b5c46ae86c9d01fce7f3552e2713c13
gbbojda <bojdait@gmail.com>
2023-05-30 22:56:16 +0200

    4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+
+    const response = await axios.get('http://localhost:3001/books');
+
+     setBooks(response.data);
+  };
+
+  useEffect(() => {
+   fetchBooks();
+  }, []);
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
@@ -17,6 +28,8 @@ function App() {
 
     setBooks(updatedBooks);
   };
+
+  
 
   const deleteBookById = (id) => {
     const updatedBooks = books.filter((book) => {
@@ -31,15 +44,12 @@ function App() {
       title
     });
 
-    console.log(response)
-    // const updatedBooks = [
-    //   ...books,
-    //   {
-    //     id: Math.round(Math.random() * 9999),
-    //     title,
-    //   },
-    // ];
-    // setBooks(updatedBooks);
+   
+    const updatedBooks = [
+      ...books,
+      response.data
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
