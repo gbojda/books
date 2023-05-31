d434c14f852cf250101a26944d732e38edb1fc77
gbbojda <bojdait@gmail.com>
2023-05-31 11:55:05 +0200

    5

--- src/App.js
+++ src/App.js
@@ -17,10 +17,16 @@ function App() {
    fetchBooks();
   }, []);
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`,{
+    title: newTitle
+  });
+  
+  console.log(response);
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
@@ -31,7 +37,10 @@ function App() {
 
   
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
